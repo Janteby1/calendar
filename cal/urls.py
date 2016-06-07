5eac30ff42c308571646a8be255a499cd1df098e
Jack Anteby <Janteby1@gmail.com>
2016-06-07 16:17:55 -0400

    starting outline code

--- cal/urls.py
+++ cal/urls.py
@@ -4,11 +4,11 @@ from cal import views #gets all our view functions
 
 urlpatterns = [
     # url(r'^$', views.Landing.as_view(), name='landing'),
-    url(r'^index$', views.Index.as_view(), name='index'),
+    url(r'^$', views.Index.as_view(), name='index'),
 
-    # url(r'^register$', views.User_Register.as_view(), name='register'),
-    # url(r'^login$', views.User_Login.as_view(), name='login'),
-    # url(r'^logout$', views.User_Logout.as_view(), name='logout'),
+    url(r'^register$', views.User_Register.as_view(), name='register'),
+    url(r'^login$', views.User_Login.as_view(), name='login'),
+    url(r'^logout$', views.User_Logout.as_view(), name='logout'),
 
     # url(r'^add$', views.AddDate.as_view(), name='add'),
     # url(r'^price$', views.SearchDate_Price.as_view(), name='search_price'),
--- cal/views.py
+++ cal/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.http import HttpResponse, HttpResponseForbidden
+from django.http import JsonResponse
+import json
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+
+from .models import UserProfile, Events, Organization, Tags, TaggedTag
+
 
 # Create your views here.
+class Landing(View):
+    def get(self, request):
+        return render(request, "landing.html")
+
+class Index(View):
+    def get(self, request):
+        # user_creation_form = UserForm()
+        # login_form = LoginForm()
+        # search_date_form_category = SearchDateForm_Category()
+        # search_date_form_area = SearchDateForm_Area()
+        # search_date_form_price = SearchDateForm_Price()
+
+        # context = {
+        #     'user_creation_form': user_creation_form,
+        #     'login_form': login_form,
+        #     "search_date_form_category":search_date_form_category,
+        #     "search_date_form_area":search_date_form_area,
+        #     "search_date_form_price": search_date_form_price,
+        #     }
+
+        return render(request, "index.html")
+
+
+class User_Register(View):
+    def post(self, request):
+        if request.is_ajax():
+            data = request.POST
+        else:
+            body = request.body.decode()
+            if not body: 
+                return JsonResponse ({"response":"Missing Body"})
+            data = json.loads(body)
+
+        user_form = UserForm(data)
+        if user_form.is_valid():
+            user = user_form.save()
+            return JsonResponse({"Message": "Register succesfull", "success": True})
+        else:
+            return JsonResponse ({"response":"Invalid information", 'success' : False, 'errors': user_form.errors })
+
+
+class User_Login(View):
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            request.session.set_expiry(30000)
+            return JsonResponse({"username":user.username, "success": True})
+        else:
+            return JsonResponse({'errors': form.errors})
+
+
+class User_Logout(View):
+    def post(self, request):
+        print(request)
+        logout(request) # django built in logout 
+        return JsonResponse ({"Message":"Logout Successful"})
+
+
+
